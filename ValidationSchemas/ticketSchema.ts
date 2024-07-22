d658070161926ca4343c6fc29d63a775fd6a7830
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-07-22 22:35:04 +0530

    ticket create api

---
+++ ValidationSchemas/ticketSchema.ts
@@ -0,0 +1,9 @@
+import { Priority, Status } from "@prisma/client";
+import { z } from "zod";
+
+export const ticketSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required.").max(65535),
+  status: z.nativeEnum(Status).optional(),
+  priority: z.nativeEnum(Priority).optional(),
+});
---
+++ app/api/tickets/route.ts
@@ -0,0 +1,23 @@
+import { ticketSchema } from "@/ValidationSchemas/ticketSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const result = ticketSchema.safeParse(body);
+
+  if (!result.success) {
+    let zoderrors = {};
+    result.error.issues.forEach(
+      (issue) => (zoderrors = { ...zoderrors, [issue.path[0]]: issue.message }),
+    );
+    return NextResponse.json(
+      { success: false, errors: zoderrors },
+      { status: 400 },
+    );
+  }
+
+  const newTicket = await prisma.ticket.create({ data: { ...result.data } });
+  return NextResponse.json({ success: true, data: newTicket }, { status: 201 });
+}
