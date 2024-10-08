efe90017f50b64c86b9f803ba3600d8c6a9b3483
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-10-08 10:41:24 +0530

    dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="grid gap-4 md:grid-cols-3 md:gap-8">
+      <div className="flex flex-col gap-4 md:col-span-2 md:gap-8">
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+          <Card className="min-h-28"></Card>
+          <Card className="min-h-28"></Card>
+          <Card className="min-h-28"></Card>
+        </div>
+        <Card className="min-h-96"></Card>
+      </div>
+      <Card className="min-h-96"></Card>
     </div>
   );
 }
