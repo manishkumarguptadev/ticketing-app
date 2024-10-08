f54d86339c2345353c09436164b12d94c6e6017b
manishkumarguptadev <manishkumarguptadev@gmail.com>
2024-10-08 12:34:03 +0530

    dashboard ticketsummary

---
+++ app/TicketSummary.tsx
@@ -0,0 +1,39 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Status } from "@prisma/client";
+import Link from "next/link";
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
+function TicketSummary({ open, inProgress, closed }: Props) {
+  const containers: {
+    label: string;
+    value: number;
+    status: Status;
+  }[] = [
+    { label: "Open Tickets", value: open, status: "OPEN" },
+    { label: "In Progress Tickets", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Tickets", value: closed, status: "CLOSED" },
+  ];
+  return (
+    <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+      {containers.map((container) => (
+        <Card className="min-h-28" key={container.status}>
+          <CardHeader>
+            <CardTitle className="pb-2 text-center text-sm font-medium">
+              <Link href={`/tickets?status=${container.status}`}>
+                {container.label}
+              </Link>
+            </CardTitle>
+            <div className="text-center text-3xl font-bold">
+              {container.value}
+            </div>
+          </CardHeader>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+export default TicketSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import { Card } from "@/components/ui/card";
+import TicketSummary from "./TicketSummary";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/_options";
+import NotAuthorized from "@/components/not-authorized";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (!(session?.user.role === "ADMIN")) return <NotAuthorized />;
+
+  const open = await prisma.ticket.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.ticket.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.ticket.count({
+    where: { status: "CLOSED" },
+  });
   return (
     <div className="grid gap-4 md:grid-cols-3 md:gap-8">
       <div className="flex flex-col gap-4 md:col-span-2 md:gap-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-          <Card className="min-h-28"></Card>
-          <Card className="min-h-28"></Card>
-          <Card className="min-h-28"></Card>
-        </div>
+        <TicketSummary open={open} inProgress={inProgress} closed={closed} />
         <Card className="min-h-96"></Card>
       </div>
       <Card className="min-h-96"></Card>
