6fa5d9ede6c12bce15af7593f84aea1f664301e5
manishkumarguptadev <manishkumarguptadev@gmail.com>
2025-03-12 20:20:41 +0530

    dashboard ticketsummary

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import { Card } from "@/components/ui/card";
+import prisma from "@/prisma/client";
 
-export default function Home() {
+import NotAuthorized from "@/components/not-authorized";
+import { auth } from "@/auth";
+import TicketSummary from "./TicketSummary";
+
+export default async function Home() {
+  const session = await auth();
+  if (!(session?.user.role === "ADMIN")) return <NotAuthorized />;
+
+  const open = await prisma.ticket.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.ticket.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.ticket.count({
+    where: { status: "CLOSED" },
+  });
   return (
     <div className="grid gap-4 md:grid-cols-3 md:gap-8">
       <div className="flex flex-col gap-4 md:col-span-2 md:gap-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
-          <Card className="min-h-28"></Card>
-          <Card className="min-h-28"></Card>
-          <Card className="min-h-28"></Card>
-        </div>
+        <TicketSummary open={open} inProgress={inProgress} closed={closed} />
         <Card className="min-h-96"></Card>
       </div>
       <Card className="min-h-96"></Card>
